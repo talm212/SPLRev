a3ed8554bbfe3df924081abd76dec6f21ea7b00e
Tal Mishali <talmish@post.bgu.ac.il>
2016-11-24 00:49:24 +0200

    hi

--- Player.cpp
+++ Player.cpp
@@ -4,3 +4,36 @@
 #include "Player.h"
 
 
+Card *Player::getTheCardThatApersTheList() {
+	return nullptr;
+}
+
+string Player::getName() {
+	return name;
+}
+
+void Player::removeCardsFromHand(vector<Card *> cards) {
+	for(std::vector<Card*>::iterator it = cards.begin(); it != cards.end(); ++it) {
+		Hand::removeCard(**it);
+	}
+}
+
+void Player::addCardsToHand(vector<Card *> cards) {
+	for (std::vector<Card *>::iterator it = cards.begin(); it != cards.end(); ++it) {
+		Hand::addCard(**it);
+	}
+}
+
+Card *Player::getTheGreatestCard() {
+	return nullptr;
+}
+
+Card *Player::getTheLowestCard() {
+	return nullptr;
+}
+
+Card *Player::getTheCardThatApearsTheMost() {
+	return nullptr;
+}
+
+
